807869f45115f1ab5c30906a5dbf05cc12da168f
Ccarretero <carloscarretero2314@gmail.com>
2025-04-26 14:58:42 -0500

    Primer commit - Micro-SaaS de conversor

---
+++ App.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def convert_units(value, from_unit, to_unit):
+    conversions = {
+        'meter': {'kilometer': value / 1000, 'centimeter': value * 100},
+        'kilogram': {'gram': value * 1000, 'pound': value * 2.20462},
+        'celsius': {'fahrenheit': (value * 9/5) + 32, 'kelvin': value + 273.15}
+    }
+    return conversions.get(from_unit, {}).get(to_unit, "Conversión no válida")
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    result = None
+    if request.method == 'POST':
+        try:
+            value = float(request.form['value'])
+            from_unit = request.form['from_unit']
+            to_unit = request.form['to_unit']
+            result = convert_units(value, from_unit, to_unit)
+        except:
+            result = "Error en la entrada"
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
